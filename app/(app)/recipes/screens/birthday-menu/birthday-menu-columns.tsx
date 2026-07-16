1877085f676c2ffd4b561def7341585f172accb6
Bastian <badmaxyt@gmail.com>
2026-07-17 00:33:18 +0200

    style(recipes): remove Crackers sésame from aperitif 2 columns menu defaults

--- app/(app)/recipes/screens/birthday-menu/birthday-menu-columns.tsx
+++ app/(app)/recipes/screens/birthday-menu/birthday-menu-columns.tsx
@@ -46,7 +46,6 @@ export default function BirthdayMenuColumns({
 		params?.feuillete1 || "Spirales au Pesto",
 		params?.feuillete2 || "Sacristains",
 		params?.feuillete3 || "Anchois",
-		params?.feuillete4 || "Crackers sésame",
 	].filter(Boolean);
 
 	const tartelettes = [
